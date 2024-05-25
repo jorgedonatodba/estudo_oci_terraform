65645f7cf367b910a2e901066e9d770235587781
Jorge Donato <jorge.donato@live.com>
2024-05-25 17:30:35 +0000

    updt

--- 01/variables-3-terraform-tfvars/oci_provider.tf
+++ 01/variables-3-terraform-tfvars/oci_provider.tf
@@ -7,11 +7,10 @@ variable "region"               { type = string }
 variable "root_compartment_id"  { type = string }
 
 
-# Resources
 provider "oci" {
+  region           = var.region
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  private_key_path = var.private_key_path
   fingerprint      = var.fingerprint
-  region           = var.region
+  private_key_path = var.private_key_path
 }
